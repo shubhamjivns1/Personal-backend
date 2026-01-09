e84e4cb4042fb3ffcca51ace692b17c107f42089
shubhamjivns1 <shubhamjivns1@gmail.com>
2026-01-09 22:19:51 +0530

    update backend console

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(process.env.PORT, () =>
-    console.log(`🚀 Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
