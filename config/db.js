e572a87d90ddf3ed0fc375c750f1571cd43a86c8
shubhamjivns1 <shubhamjivns1@gmail.com>
2026-01-10 10:53:55 +0530

    server sleep

--- config/db.js
+++ config/db.js
@@ -3,12 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createPool({
+export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
+
 
 db.getConnection()
     .then(() => console.log("✅ MySQL Connected"))
